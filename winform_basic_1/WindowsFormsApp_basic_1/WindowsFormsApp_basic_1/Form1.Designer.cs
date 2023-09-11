48c6f358a6e2733e9fdead04b962e48510e23398
Hangyeol Kim <gksrufkim100@gmail.com>
2023-09-11 22:27:54 +0900

    버튼 클릭 이벤트 처리

---
+++ winform_basic_1/WindowsFormsApp_basic_1/WindowsFormsApp_basic_1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsApp_basic_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_click = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_click
+            // 
+            this.btn_click.Location = new System.Drawing.Point(326, 312);
+            this.btn_click.Name = "btn_click";
+            this.btn_click.Size = new System.Drawing.Size(140, 48);
+            this.btn_click.TabIndex = 0;
+            this.btn_click.Text = "Click";
+            this.btn_click.UseVisualStyleBackColor = true;
+            this.btn_click.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_click);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_click;
+    }
+}
+
---
+++ winform_basic_1/WindowsFormsApp_basic_1/WindowsFormsApp_basic_1/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_basic_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("버튼이 클릭됨!");
+        }
+    }
+}
